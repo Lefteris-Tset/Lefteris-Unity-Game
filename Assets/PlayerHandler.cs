9da163d56361e26979a03f8bd364aa8346d54899
Lefteris-Tset <tsetiroklislefteris@gmail.com>
2025-12-07 18:59:59 +0200

    Initial commit with Unity project

---
+++ Assets/PlayerHandler.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField] private float playerSpeed;
+
+    // Jump variables
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private int points = 0;
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rb.MovePosition(rb.position + movement * playerSpeed * Time.fixedDeltaTime);
+
+        isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, groundCheckDistance, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) { Jump(); }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    internal void IncreasePoints()
+    {
+        points++;
+    }
+}
