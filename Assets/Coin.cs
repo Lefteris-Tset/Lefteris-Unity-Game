2cc3ff0c9829388197bd901702ba92cc7617bc4d
Lefteris Tsetiroklis <tsetiroklislefteris@gmail.com>
2025-12-15 15:44:59 +0200

    Coin.cs

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private float spinSpeed = 180f;
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         PlayerHandler ph = other.GetComponent<PlayerHandler>();
