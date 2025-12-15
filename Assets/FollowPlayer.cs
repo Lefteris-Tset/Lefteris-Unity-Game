14fd9ab0d4e789783ea4e625ca93986c0649a166
Lefteris Tsetiroklis <tsetiroklislefteris@gmail.com>
2025-12-15 16:01:55 +0200

    FollowPlayer.cs

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -5,7 +5,6 @@ public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform cameraTarget;
     [SerializeField] private Vector3 offset;
-
     [SerializeField] private float smoothTime = 0.25f;
     
     private Vector3 velocity= Vector3.zero;
